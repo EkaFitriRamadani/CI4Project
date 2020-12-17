033c13651c10e9e802a7ae08777335a9d5318a2d
Eka Fitri Ramadani <eka.efira@gmail.com>
2020-12-17 22:09:04 +0800

    Update

---
+++ seminar/app/Views/komik/index.php
@@ -0,0 +1,40 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <a href="/komik/create" class="btn mt-3 text-light" style="background-color: #F08080; font-family: cursive;">Tambah Data Komik</a>
+            <h1 class="mt-2" style="font-family: cursive; color: lightcoral;"><center><b>Daftar Komik</b></center></h1>
+            <?php if (session()->getFlashdata('pesan')) : ?>
+                <div class="alert alert-success" role="alert">
+                    <?= session()->getFlashdata('pesan'); ?>
+                </div>
+            <?php endif; ?>
+            <table class="table" style="background-color: darkgrey; font-family: cursive;">
+                <thead class="bg-secondary text-light">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Sampul</th>
+                        <th scope="col">Judul</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($komik as $k) : ?>
+                        <tr>
+                            <th scope="row"><?= $i++; ?></th>
+                            <td><img src="/img/<?= $k['sampul']; ?>" alt="" class="sampul"></td>
+                            <td><?= $k['judul']; ?></td>
+                            <td>
+                                <a href="/komik/<?= $k['slug']; ?>" class="btn text-light" style="background-color: #CD5C5C;">Detail</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
